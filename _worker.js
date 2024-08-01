f60bf370f4ea032d7aac6c49f0f65b6ff37dde39
John <1028382160@qq.com>
2024-08-01 11:15:28 +0800

    修改优选地址api

--- _worker.js
+++ _worker.js
@@ -11,6 +11,10 @@ let addresses = [
 // 设置优选地址api接口
 let addressesapi = [
     'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
+    'https://ct.xxxxxxxx.tk/',
+    'https://ipdb.api.030101.xyz/?type=bestcf&country=true',
+    'https://addressesapi.090227.xyz/CloudFlareYes',
+    'https://addressesapi.090227.xyz/ct'
 ];
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
